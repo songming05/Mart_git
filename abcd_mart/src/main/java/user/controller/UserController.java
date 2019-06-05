5ae2e43e209a93c4cf930194edc72d7551ec6dbd
insungHwag <dlstjd5913@gmail.com>
2019-06-05 15:44:01 +0900

    id찾기 아침에 바꾼거로 복귀했습니다

--- abcd_mart/src/main/java/user/controller/UserController.java
+++ abcd_mart/src/main/java/user/controller/UserController.java
@@ -121,9 +121,4 @@ public class UserController {
 		return result;
 	}
 	
-	@RequestMapping(value = "/idFounds", method = RequestMethod.GET)
-	public String idFounds(HttpSession session) {
-		session.invalidate();
-		return "/user/idFound";
-	}
 }
