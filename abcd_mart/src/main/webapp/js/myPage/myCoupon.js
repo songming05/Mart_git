4ef8a859ebb5a91d22d3ca85d665b99d11c06b67
songming05 <songming05@gmail.com>
2019-06-10 10:11:54 +0900

    js

--- abcd_mart/src/main/webapp/js/myPage/myCoupon.js
+++ abcd_mart/src/main/webapp/js/myPage/myCoupon.js
@@ -6,6 +6,37 @@ $(document).ready(function() {
 		dataType:'json',
 		success: function(data) {
 			console.log(data);
+			
+			$('.group-center').empty();
+			//6개넘어옴  each 돌때마다 items에 담김
+			$.each(data.list , function(index, items){
+				$('<td/>',{
+					class:'tit_type3',
+					text: items.
+				}).append($('<td/>',{
+					align : 'center',
+					text : items.prdtCode
+				})).append($('<td/>',{
+					align : 'center',
+					text : items.prdtQty
+				})).append($('<td/>',{
+					align : 'center',
+					text : items.prdtBrand
+				})).append($('<td/>',{
+					align : 'center',
+					text : items.prdtColor
+				})).append($('<td/>',{
+					align : 'center',
+					text : items.prdtGender
+				})).append($('<td/>',{
+					align : 'center',
+					text : items.prdtPrice
+				})).append($('<td/>',{
+					align : 'center',
+					text : items.prdtSize
+				})).appendTo($('.group-center'));
+				
+			});//each
 		}
 	});
 });
